e5b68f21cad3975eb03a6a459de911416680e35e
steveg152 <steve.greco2@gmail.com>
2024-05-14 21:59:15 -0400

    removed auto docs

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -24,8 +24,8 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  // docs: {
+  //   autodocs: "tag",
+  // },
 };
 export default config;
