518172a5495cf94189f3e4be6dcf551d027a0b0a
steveg152 <steve.greco2@gmail.com>
2024-10-14 08:45:07 -0400

    Trying to add to the head

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -52,6 +52,13 @@ const config: StorybookConfig = {
       },
     },
   },
+  previewHead: (head) => `
+  ${head}
+  <script>
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+  </script>
+  <script defer src="/_vercel/insights/script.js"></script>
+  `,
 };
 export default config;
 
