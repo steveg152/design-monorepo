b2f00d25087820ad6ef654244191e9fdbb2003d5
steveg152 <steve.greco2@gmail.com>
2024-10-14 09:03:25 -0400

    Trying decorator

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
 import "@repo/ui/tailwind.css";
 import "@repo/ui-react-aria/dist/tailwind.css";
 import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 inject();
 
@@ -17,6 +19,13 @@ const preview: Preview = {
   },
 
   decorators: [
+    (Story) => (
+      <div style={{ margin: "3em" }}>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+        <Analytics />
+      </div>
+    ),
     withThemeByDataAttribute({
       defaultTheme: "dark",
       themes: {
